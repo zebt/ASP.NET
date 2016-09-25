00ac439f3ebcc22037fdc586bf30f69f08081a60
zebt <azzabrocki@gmail.com>
2016-09-25 17:53:25 +0200

    Our first content defined in action, displayed in view

--- ASP.NET/App_Start/BundleConfig.cs
+++ ASP.NET/App_Start/BundleConfig.cs
@@ -24,7 +24,7 @@ namespace ASP.NET
                       "~/Scripts/respond.js"));
 
             bundles.Add(new StyleBundle("~/Content/css").Include(
-                      "~/Content/bootstrap.css",
+                      "~/Content/bootstrap-lumen.css",
                       "~/Content/site.css"));
         }
     }
